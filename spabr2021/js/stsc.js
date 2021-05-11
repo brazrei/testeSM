2ffa451f5d59656a42993904a5d3f6990002a354
brazrei <79328469+brazrei@users.noreply.github.com>
2021-05-11 12:29:51 +0100

    Add files via upload

--- spabr2021/js/stsc.js
+++ spabr2021/js/stsc.js
@@ -12,6 +12,12 @@ var centroAvisoSTSCTMASP = [["-23.50", "-046.63"], // SBMT
 ["-23.00", "-047.13"] //SBKP
 ]
 
+var boolAlertaSTSCTMASP = boolAlertaSTSCTMABH = boolAlertaSTSCTMACT = boolAlertaSTSCTMARJ = false
+
+var coordTMARJ = "S2238 W04338 - S2236 W04301 - S2303 W04238 - S2314 W04327 - S2238 W04338"
+var coordTMABH = "S1917 W04422 - S1913 W04414 - S1909 W04356 - S1929 W04329 - S1953 W04331 - S2005 W04347 - S1958 W04418 - S1931 W04425 - S1917 W04422"
+var coordTMASP = "S2245 W04711 - S2320 W04607 - S2352 W04630 - S2333 W04704 - S2300 W04724 - S2245 W04711"
+var coordTMACT = "S2510 W04920 - S2523 W04855 - S2552 W04903 - S2541 W04934 - S2521 W04928 - S2510 W04920"
 //var centroAvisoSTSC = ["-10.87","-061.84"] // SBji
 
 
@@ -19,21 +25,60 @@ var arrayteste = []
 
 //var stscCenterMap=[];
 
+function detectSTSC(coordEdit,lat,long) {
+    // let coordEdit = $("#taCoordenadas").val()
+
+    let poly = turf.polygon([getCoordDegAirmet(coordEdit)]);
+
+    return (turf.booleanPointInPolygon([long, lat], poly))
+        //for (let i in arrCutted)
+    //    formataLayerEdit(arrCutted[i])
+    /*stscAneis.forEach(anel => {
+        if (turf.booleanPointInPolygon([anel[1], anel[0]], poly)) {
+            cont++
+        }
+    });
+    return cont*/
+}
+
 function inAreaAvisoSTSC(lat, long) {
 
+    //TMA-SP
+    if (!boolAlertaSTSCTMASP) {
+        let distancia
+        for (let i in centroAvisoSTSCTMASP) {
+            distancia = getDistancia([long, lat], [centroAvisoSTSCTMASP[i][1], centroAvisoSTSCTMASP[i][0]])
+            boolAlertaSTSCTMASP = (distancia <= raioAvisoSTSC)
+            if (boolAlertaSTSCTMASP)
+                return {ativo: true, TMA: "TMASP"}
+        }
+    }
 
-    let distancia
-    centroAvisoSTSCTMASP.forEach(centroAvisoSTSC => {
-        let oldDist = distancia
-        distancia = getDistancia([long, lat], [centroAvisoSTSC[1], centroAvisoSTSC[0]])
-        if (oldDist <= distancia)
-            distancia = oldDist
-    })
+    //OUTRAS TERMINAIS
+
+    if (!boolAlertaSTSCTMABH) {
+        boolAlertaSTSCTMABH = detectSTSC(coordTMABH,lat,long)
+        if (boolAlertaSTSCTMABH)
+            return {ativo: true, TMA: "TMABH"}
+    }
+
+    if (!boolAlertaSTSCTMACT) {
+        boolAlertaSTSCTMACT = detectSTSC(coordTMACT,lat,long)
+        if (boolAlertaSTSCTMACT)
+            return {ativo: true, TMA: "TMACT"}
+    }
+
+    if (!boolAlertaSTSCTMARJ) {
+        boolAlertaSTSCTMARJ = detectSTSC(coordTMARJ,lat,long)
+        if (boolAlertaSTSCTMARJ)
+            return {ativo: true, TMA: "TMARJ"}
+    }
 
     //arrayteste.push(distancia)
 
-    return distancia < raioAvisoSTSC
+    return {ativo: false, TMA: ""}
 }
+
 function animaSTSC() {
     if (!heat || heat.length == 0)
         return;
@@ -63,11 +108,32 @@ function animaSTSC() {
     intervalAnimaSTSC = setTimeout('animaSTSC()', intervalo);
 }
 
-function updateAlertaSTSC(on) {
+function clearGridTMAs(){
+    boolAlertaSTSCTMASP = boolAlertaSTSCTMABH = boolAlertaSTSCTMACT = boolAlertaSTSCTMARJ = false
+    allTMAs = ["TMABH","TMACT","TMARJ","TMASP"]
+    for (let i in allTMAs){
+        $(".grid"+allTMAs[i]).removeClass("pulse")
+        $(".grid"+allTMAs[i]).css("background-color", "green");
+    }
+}
+
+function updateGridSTSC(TMAs){
+    for (let i in TMAs){
+        $(".grid"+TMAs[i]).addClass("pulse")
+        $(".grid"+TMAs[i]).css("background-color", "red");
+    }
+}
+
+function updateAlertaSTSC(on, TMAs) {
     if (on) {
-        $("#labelAvisoSTSC").show()
-    } else
+        $("#labelAvisoSTSC").show();
+        $("#labelAvisoSTSC").css("color", "red");
+        $(".grid").show()
+        updateGridSTSC(TMAs)
+    } else{
         $("#labelAvisoSTSC").hide()
+        $(".grid").hide()
+    }
 
 }
 
@@ -117,11 +183,11 @@ function plota_stsc(obj_chk) {
                 let diffHora = dif.getHours()
                 let diffMin = dif.getMinutes()
 
-                if (isLinux()){ 
+                if (isLinux()) {
                     diffHora -= 21; //decrementa 1 hora, bug do linux
                 }
 
-                diffMin += (diffHora*60)
+                diffMin += (diffHora * 60)
                 let erro = (diffMin > 20) //20 minutos
 
                 formataErro('#clockSTSC', erro)
@@ -131,14 +197,20 @@ function plota_stsc(obj_chk) {
                     var pontos = data.data.stsc[i];
                     let stscAneis = []
                     alertaSTSC = false
+                    TMAs = []
+                    clearGridTMAs()
                     for (var key in pontos) {
                         stscAneis.push([pontos[key].la, pontos[key].lo, 1]);
-                        if (i == data.data.stsc.length - 1)//só busca na mais recente
-                            alertaSTSC = alertaSTSC || inAreaAvisoSTSC(pontos[key].la, pontos[key].lo)
+                        if (i == data.data.stsc.length - 1) {//só busca na mais recente
+                            let checkArea = inAreaAvisoSTSC(pontos[key].la, pontos[key].lo)
+                            if (checkArea.TMA!=="" && !TMAs.includes(checkArea.TMA))
+                                TMAs.push(checkArea.TMA)
+                            alertaSTSC = alertaSTSC || checkArea.ativo
+                        }
                         // i++;
                     }
 
-                    updateAlertaSTSC(alertaSTSC)
+                    updateAlertaSTSC(alertaSTSC, TMAs)
                     xheat.push(L.heatLayer(stscAneis, {
                         radius: 5,
                         blur: 1,
@@ -156,18 +228,18 @@ function plota_stsc(obj_chk) {
                     heat = []
                 else {
                     try {
-                        if (heat.length>0){ 
-                            for (let i in heat){
-                              heat[i].layer.removeFrom(map);
+                        if (heat.length > 0) {
+                            for (let i in heat) {
+                                heat[i].layer.removeFrom(map);
                             }
                             heat = []
                         }
-                    } catch(e){
+                    } catch (e) {
 
                     }
 
                 }
-                
+
                 heat.push({ layer: xheat[xheat.length - 1], hora: horaAnima })
                 if (heat.length > 10)
                     heat = heat.slice(1)
@@ -233,3 +305,4 @@ function canvasFadeIn(canvas) {
         delta = 0.1; /// delta = speed
 
 }
+
