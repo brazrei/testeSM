8aadd795b6621f4dd9c640782ab16fa00a7963db
brazrei <79328469+brazrei@users.noreply.github.com>
2021-05-13 22:29:50 +0000

    Update stsc.js

--- spabr2021/js/stsc.js
+++ spabr2021/js/stsc.js
@@ -248,6 +248,9 @@ function plota_stsc(obj_chk) {
                         xheat.push(L.heatLayer(stscAneis, optDefault));
                 }
 
+                if (xheat.length>0)
+                  heat.push(xheat[0])
+                
                 if (!heat) // dessabilita a animacao
                     heat = []
                 else {
