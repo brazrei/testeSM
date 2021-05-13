0daa65eb8cbabcab2e97367ccb6dfbc6b2016867
brazrei <79328469+brazrei@users.noreply.github.com>
2021-05-13 23:09:47 +0000

    Update stsc.js

--- spabr2021/js/stsc.js
+++ spabr2021/js/stsc.js
@@ -30,12 +30,12 @@ optDefault = {
     blur: 1,
     maxZoom: 8,
     opacity: 1,
-    minOpacity: 0.1,
-    gradient: {
+    minOpacity: 0.1
+    /*gradient: {
         0.0: 'green',
         0.5: 'yellow',
         1.0: 'red'
-    }
+    }*/
 }
 
 
