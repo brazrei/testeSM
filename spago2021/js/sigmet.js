d8ba0389a5a521c1451e5ab39e259a9a0529abd5
brazrei <79328469+brazrei@users.noreply.github.com>
2021-09-18 00:12:14 +0100

    Update sigmet.js

--- spago2021/js/sigmet.js
+++ spago2021/js/sigmet.js
@@ -324,7 +324,7 @@ function getSigmet(primeiraVez = false) {
     dfim = "2020101815"
     var interval = ""
     //var interval = `&data_ini=${dini}&data_fim=${dfim}`
-    var url = "https://www.redemet.aer.mil.br/api/consulta_automatica/index.php?local=SBAZ,SBBS,SBRE,SBAO,SBCW&msg=sigmet" + interval;
+    var url = "https://www.redemet.intraer/api/consulta_automatica/index.php?local=SBAZ,SBBS,SBRE,SBAO,SBCW&msg=sigmet" + interval;
     //  var url = "https://www.redemet.aer.mil.br/api/consulta_automatica/index.php?local=SBAZ,SBBS,SBRE,SBCW&msg=sigmet&data_ini="+dataIni+"&data_fim="+dataFim;
 
     GetWebContentSigmet(url, primeiraVez);
