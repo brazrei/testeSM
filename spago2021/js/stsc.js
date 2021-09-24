21186088f4ef783c2311f2c373a930437d9e1ceb
brazrei <79328469+brazrei@users.noreply.github.com>
2021-09-24 12:40:25 +0000

    Update stsc.js

--- spago2021/js/stsc.js
+++ spago2021/js/stsc.js
@@ -264,7 +264,7 @@ function plota_stsc(obj_chk) {
             loadPHP_STSC()
         }
         else
-        url = 'https://www.redemet.intraer/api/produtos/stsc?api_key=U9Q2PoK6e5uhykrMXrsrGAQssG8htAnPIqXsxmei';
+          url = 'https://www.redemet.intraer/api/produtos/stsc?api_key=U9Q2PoK6e5uhykrMXrsrGAQssG8htAnPIqXsxmei';
 //        url = 'https://api-redemet.decea.gov.br/api/produtos/stsc?api_key=U9Q2PoK6e5uhykrMXrsrGAQssG8htAnPIqXsxmei';
 
         $.ajax({
