28632c6dc011b8f9ac6e492b105d68defb5e31b9
brazrei <79328469+brazrei@users.noreply.github.com>
2021-10-04 12:55:05 +0000

    Update top-cache.php

--- ago2021/php/top-cache.php
+++ ago2021/php/top-cache.php
@@ -2,13 +2,13 @@
   $url = $_SERVER["SCRIPT_NAME"];
   $break = Explode('/', $url);
   $file = $break[count($break) - 1];
-  $encrypted_Locs = hash('sha256',$_POST['url']);
+  $encrypted_url = hash('sha256',$_POST['url']);
 
   $dirName = "cache";
   if (!file_exists($dirName)) {
      mkdir($dirName, 0777);
   }
-  $cachefile = $dirName . "/" . 'cached-'.substr_replace($file ,"",-4).$encrypted_Locs.'.html';
+  $cachefile = $dirName . "/" . 'cached-'.substr_replace($file ,"",-4).$encrypted_url.'.html';
   $cachetime = 55;
 
   // Serve from the cache if it is younger than $cachetime
