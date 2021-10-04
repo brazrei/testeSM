7f45ead0852d50a748ebcfaf5abfe7671415fa37
brazrei <79328469+brazrei@users.noreply.github.com>
2021-10-04 15:19:18 +0000

    Update top-cache.php

--- ago2021/php/top-cache.php
+++ ago2021/php/top-cache.php
@@ -6,7 +6,7 @@
   //$teste = "http://www.redemet.intraer//api/consulta_automatica/index.php?local=SBEG,SBMN,SBBV,SBPV,SBRB,SBCY,SBSL,SBBE,SBJC,SBSN,SBMQ,SBCZ,SBTF,SBMY,SBAT,SBUA,SBCC,SBSO,SBIH,SBTT,SBTK,SBJI,SBHT,SBMA,SBVH,SBTU,SBOI,SBCJ,SBCI,SBIZ,SBTS,SBTB,SBUY,SBIC,SBEK,SBGM,SBMD,SBAA,SBRD,SSKW,SBSI&msg=metar";
   //$encrypted_url = hash('sha256',$_POST['urlConsulta']);
   //$urlF = $teste;
-  $urlF = $_POST['urlConsulta']."";
+  $urlF = $_POST['testeParam']."";
   $encrypted_url = hash('sha256',$urlF);
 
   $dirName = "cache";
