36689ee36dde7771c44eaa4cf10d73de898559bf
brazrei <79328469+brazrei@users.noreply.github.com>
2021-10-04 12:32:01 +0000

    Update bottom-cache.php

--- ago2021/php/bottom-cache.php
+++ ago2021/php/bottom-cache.php
@@ -1,7 +1,12 @@
 <?php
 // Cache the contents to a cache file
-$cached = fopen($cachefile, 'w');
-fwrite($cached, ob_get_contents());
-fclose($cached);
-ob_end_flush(); // Send the output to the browser
+  $dirName = "cache";
+  if (!file_exists($dirName)) {
+     mkdir($dirName, 0777);
+  }
+  $cachefile = $dirName . "/" . $cachefile;
+  $cached = fopen($cachefile, 'w');
+  fwrite($cached, ob_get_contents());
+  fclose($cached);
+  ob_end_flush(); // Send the output to the browser
 ?>
