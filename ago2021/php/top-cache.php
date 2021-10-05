a92dab3cbe8569a6dc965c7d8c58d50cd6154470
brazrei <79328469+brazrei@users.noreply.github.com>
2021-10-05 17:19:47 +0000

    Update top-cache.php

--- ago2021/php/top-cache.php
+++ ago2021/php/top-cache.php
@@ -1,4 +1,15 @@
 <?php
+  function makeArgs() {
+    $args = "";
+    $sep = "";
+    foreach ($_GET as $key => $value) {
+      if ($key !== "url") {
+        $args = $sep . $key . "=" . $value;
+        $sep = "&";
+      }
+    }
+    return $args;
+  }
   $url = $_SERVER["SCRIPT_NAME"];
   $break = Explode('/', $url);
   $file = $break[count($break) - 1];
@@ -6,9 +17,11 @@
   //$teste = "http://www.redemet.intraer//api/consulta_automatica/index.php?local=SBEG,SBMN,SBBV,SBPV,SBRB,SBCY,SBSL,SBBE,SBJC,SBSN,SBMQ,SBCZ,SBTF,SBMY,SBAT,SBUA,SBCC,SBSO,SBIH,SBTT,SBTK,SBJI,SBHT,SBMA,SBVH,SBTU,SBOI,SBCJ,SBCI,SBIZ,SBTS,SBTB,SBUY,SBIC,SBEK,SBGM,SBMD,SBAA,SBRD,SSKW,SBSI&msg=metar";
   //$encrypted_url = hash('sha256',$_POST['urlConsulta']);
   //$urlF = $teste;
-  $urlF = $_GET['url'];
-  $msg = $_GET['msg'];
-  $urlF = $urlF . "&msg=" . $msg;
+
+  $args = makeArgs();
+  $urlF = $_GET['url'] . "?" . $args;
+  echo $urlF;
+
   $encrypted_url = hash('sha256',$urlF);
 
   $dirName = "cache";
