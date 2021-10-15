c41a19cbe34ce16da27bf4707c896bd39428a47c
brazrei <79328469+brazrei@users.noreply.github.com>
2021-10-15 10:17:43 +0000

    Update consulta_msg.php

--- ago2021/php/consulta_msg.php
+++ ago2021/php/consulta_msg.php
@@ -1,16 +1,17 @@
 <?php
   
   function getCurl($url) {
-    $ch=curl_init();
-    $timeout=45;
+    $ch = curl_init($url);
+    $timeout = 45;
 
-    curl_setopt($ch, CURLOPT_URL, $url);
+    //curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 
-    $result=curl_exec($ch);
+    $result = curl_exec($ch);
     curl_close($ch);
+    
     return $result;
   }
   include('top-cache.php'); 
