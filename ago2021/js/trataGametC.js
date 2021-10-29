16b2757c35a2a8f3a3dac0138b8324e7bf9444d4
brazrei <braz.rei@gmail.com>
2021-10-29 20:21:46 +0000

    Ajuste nas colunas do GAMET

--- ago2021/js/trataGametC.js
+++ ago2021/js/trataGametC.js
@@ -102,7 +102,7 @@ function trataGametRedemet(texto) {
         strStyle = 'style="display:none"'
 
       str = erro + "GAMET VALID " + val + " " + arrayLocalidadeFIR[idx].toUpperCase() + " - " + vis + " - " + teto;
-      $("#" + arrayTableFir[idx] + " thead").append('<tr class="' + classe + '" ' + strStyle + '><td><b>' + str + '</td><td></b></td><td></td><td></td><td></td></tr>');
+      $("#" + arrayTableFir[idx] + " thead").append('<tr class="' + classe + '" ' + strStyle + '><td colspan = "5"><b>' + str + '</b></td></tr>');
       $("#" + arrayTableFir[idx] + " thead").append('<tr class="cabecalho"><td><b>Mensagem</b></td><td><b>Status AD WRNG</b></td><td><b>Status GAMET</b></td><td><b>Status AIRMET</b></td><td><b>CMA-1</b></td></tr>');
 
       if ((gamet.indexOf("VIS") == -1) && (part1[idx] == 0)) {
