9e373129533451fb472763b945acb1587c750fb3
brazrei <79328469+brazrei@users.noreply.github.com>
2021-12-18 18:29:59 +0000

    Update airmet.js

--- spago2021/js/airmet.js
+++ spago2021/js/airmet.js
@@ -721,8 +721,8 @@ function plotaMarca(lat, lng, loc) {
             if (descU.includes("DESCOBERTO")) {
                 let strDescoberto = descU.split("DESCOBERTO")[1].split("<")[0]
                 let alerta = getTipoAlerta(loc, strDescoberto);
-                icon = redIcon
-                icon = getSvgIcon(loc, strDescoberto, adWRNGPertoDoFim, true) //vento trovoada teto visib
+                //icon = redIcon
+                icon = getSvgIcon(loc, alerta.strAlerta, adWRNGPertoDoFim, true) //vento trovoada teto visib
 
                 //if (alerta.ad)
                 addMarker(L.marker([lat, lng], { icon: cssIconRed }), "", restricao, true)
