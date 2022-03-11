b07156e9e42f720d592e5619058daa95ddf3f001
brazrei <79328469+brazrei@users.noreply.github.com>
2022-03-11 17:31:19 +0000

    Update airmet.js

--- spago2021/js/airmet.js
+++ spago2021/js/airmet.js
@@ -12,17 +12,6 @@ var groupMarkers = false
 var intervalAirmet = false
 var arrayMetaresGeral = []
 
-function addHours(data, horas) {
-    if (!Date.prototype.addHours)
-        Date.prototype.addHours = function (h) {
-            this.setHours(this.getHours() + h);
-            return this;
-        }
-
-    return data.addHours(horas)
-
-}
-
 function mostraAirmet() {
     if ($('#chkAirmet').prop('checked')) {
         getAirmet();
@@ -86,87 +75,14 @@ function getUTCAgora() {
 
 }
 
-function getFullDateValid(dataI, dataF) { // retorna data inteira de AIRMET e SIGMET 
-    let diaI = dataI.substr(0, 2);
-    let horaI = dataI.substr(2, 2);
-    let minI = dataI.substr(4, 2);
-
-    let diaF = dataF.substr(0, 2);
-    let horaF = dataF.substr(2, 2);
-    let minF = dataF.substr(4, 2);
-
-
-    //  let diaA = 
-    let agora = getUTCAgora()
-    let decMonth = 0
-    if (diaI > agora.getUTCDay())
-        decMonth = -1;
-
-    let dataInicial = new Date(agora.getUTCFullYear(), agora.getUTCMonth() + decMonth, diaI, horaI, minI)
-    let dataFinal = new Date(agora.getUTCFullYear(), agora.getUTCMonth(), diaF, horaF, minF)
-
-    // if (parseInt(diaF) < parseInt(diaI))
-    //     dataFinal.setMonth(dataFinal.getMonth() + 1);
-    //console.log(agora.toISOString())
-    //console.log(dataInicial.toISOString())
-    //console.log(dataFinal.toISOString())
-    return [dataInicial, dataFinal]
-
-}
-
-function getUTCDate(date) {
-    //let now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
-        // date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
-    let now_utc = new Date(date.getTime() + date.getTimezoneOffset()*60000)
-    return now_utc;
-}
-
 function isValidAirmet(ini, fim) {
-    let interval = getFullDateValid(ini, fim)
-    let agora = getUTCAgora()
-    if ((agora >= interval[0]) && (agora <= interval[1])) {
-        return true
-    } else if (interval[0] > agora)
-        return -1
-    return false
+    return isValidMsg(ini, fim)
 }
-function isLinux() {
+
+/*function isLinux() {
     return navigator.platform.indexOf("Linux") > -1
-}
+}*/
 
-function calcDifData(ini, fim) {
-    let dif = new Date(fim - ini)
-    let chk = new Date(new Date() - new Date())
-    if (chk < new Date(1970))
-        addHours(dif,3)
-    return dif
-}
-
-function isCloseToValidOff(ini, fim, timer = 10) {
-    if (ini == "")
-        return false
-
-    if (!fim) {
-        let valid = getValidadeAirmet(ini)
-        ini = valid.split("/")[0]
-        fim = valid.split("/")[1]
-    }
-    let agora = getUTCAgora()
-    fim = getFullDateValid(ini, fim)[1]
-
-    let restante = calcDifData(agora,fim)
-
-    if (restante.getFullYear() < 1970) {
-        addHours(restante, 3)
-        //restante.addHours(3)
-    }
-    if (restante.getHours() == 0) { // não sei porque, mas estava começando da hora 01 no Linux
-        if (restante.getMinutes() <= timer)
-            return true
-    }
-
-    return false
-}
 
 /*
 valid = isValidAirmet("291409", "291815")
@@ -177,50 +93,25 @@ console.log(isCloseToValidOff("291400", "291418"))
 */
 function getIniAirmet(hora) {
     //201200/201800
-    return hora.substr(2, 2); makeDataHoraAgora
+    return getIniDataHora(hora)
 }
 
 function getFimAirmet(hora) {
     //201200/201800
-    return hora.substr(9, 2);
+    return getFimDataHora(hora);
 }
-function getValidadeAirmet(text) {
-    var t = text.replace(/ /g, "")
-    //console.log("t => " + t )
 
-    nuvPatt2 = /\d{6}\/\d{6}/g; //PEGA O intervalo de validade de str sem espaços
-    var t1 = t.match(nuvPatt2)
-    if (t1 && t1.length > 0)
-        return t1[0]
-    else
-        return ""
+function getValidadeAirmet(text) {
+    return getValidadeMsg(text) 
 }
 
 function airmetPertoDoFim(airmet) {
-    let val = getValidadeAirmet(airmet);
-    let ini = val.split("/")[0]
-    let fim = val.split("/")[1]
-
-    return isCloseToValidOff(ini, fim)
+    return msgPertoDoFim(airmet)
 }
-
-/*function getValidadeAirmet(airmet) {
-    //VALID 201200/201800
-    return airmet.split(" VALID ")[1].substr(0, 13);
-}*/
 
 function checaValidadeAirmet(airmet) {
-
-    let val = getValidadeAirmet(airmet);
-
-    let ini = val.split("/")[0]
-    let fim = val.split("/")[1]
-    if ((ini !== "") && (fim !== ""))
-        return isValidAirmet(ini, fim)
-    else
-        return false
+    return checaValidadeMsg(airmet)
 }
-
 
 /*funcoes gerais */
 
