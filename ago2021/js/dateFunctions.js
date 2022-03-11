2284b27daf0141c13628cf2274edced9ae5a4e72
brazrei <79328469+brazrei@users.noreply.github.com>
2022-03-11 17:28:27 +0000

    Update dateFunctions.js

--- ago2021/js/dateFunctions.js
+++ ago2021/js/dateFunctions.js
@@ -1,3 +1,96 @@
+function addHours(data, horas) {
+    if (!Date.prototype.addHours)
+        Date.prototype.addHours = function (h) {
+            this.setHours(this.getHours() + h);
+            return this;
+        }
+
+    return data.addHours(horas)
+
+}
+
+function calcDifData(ini, fim) {
+    let dif = new Date(fim - ini)
+    let chk = new Date(new Date() - new Date())
+    if (chk < new Date(1970))
+        addHours(dif,3)
+    return dif
+}
+
+function isCloseToValidOff(ini, fim, timer = 10) {
+    if (ini == "")
+        return false
+
+    if (!fim) {
+        let valid = getValidadeAirmet(ini)
+        ini = valid.split("/")[0]
+        fim = valid.split("/")[1]
+    }
+    let agora = getUTCAgora()
+    fim = getFullDateValid(ini, fim)[1]
+
+    let restante = calcDifData(agora,fim)
+
+    if (restante.getFullYear() < 1970) {
+        addHours(restante, 3)
+        //restante.addHours(3)
+    }
+    if (restante.getHours() == 0) { // não sei porque, mas estava começando da hora 01 no Linux
+        if (restante.getMinutes() <= timer)
+            return true
+    }
+
+    return false
+}
+
+function getFullDateValid(dataI, dataF) { // retorna data inteira de AIRMET e SIGMET 
+    let diaI = dataI.substr(0, 2);
+    let horaI = dataI.substr(2, 2);
+    let minI = dataI.substr(4, 2);
+
+    let diaF = dataF.substr(0, 2);
+    let horaF = dataF.substr(2, 2);
+    let minF = dataF.substr(4, 2);
+
+
+    //  let diaA = 
+    let agora = getUTCAgora()
+    let decMonth = 0
+    if (diaI > agora.getUTCDay())
+        decMonth = -1;
+
+    let dataInicial = new Date(agora.getUTCFullYear(), agora.getUTCMonth() + decMonth, diaI, horaI, minI)
+    let dataFinal = new Date(agora.getUTCFullYear(), agora.getUTCMonth(), diaF, horaF, minF)
+
+    // if (parseInt(diaF) < parseInt(diaI))
+    //     dataFinal.setMonth(dataFinal.getMonth() + 1);
+    //console.log(agora.toISOString())
+    //console.log(dataInicial.toISOString())
+    //console.log(dataFinal.toISOString())
+    return [dataInicial, dataFinal]
+
+}
+
+function isValidMsg(ini, fim) {
+    let interval = getFullDateValid(ini, fim)
+    let agora = getUTCAgora()
+    if ((agora >= interval[0]) && (agora <= interval[1])) {
+        return true
+    } else if (interval[0] > agora)
+        return -1
+    return false
+}
+
+/*function isValidAirmet(ini, fim) {
+    let interval = getFullDateValid(ini, fim)
+    let agora = getUTCAgora()
+    if ((agora >= interval[0]) && (agora <= interval[1])) {
+        return true
+    } else if (interval[0] > agora)
+        return -1
+    return false
+}*/
+
 function getNum(str) {
     //return str.match(/\d+/) [0]
     return str.replace(/\D/g, '')
@@ -8,7 +101,7 @@ function isLinux() {
     return navigator.appVersion.indexOf("Linux") > -1
 }
 
-function getValidadeAirmet(text) {
+function getValidadeMsg(text) {
     var t = text.replace(/ /g, "")
     //console.log("t => " + t )
 
@@ -18,16 +111,6 @@ function getValidadeAirmet(text) {
         return t1[0]
     else
         return ""
-}
-
-function isValidAirmet(ini, fim) {
-    let interval = getFullDateValid(ini, fim)
-    let agora = getUTCAgora()
-    if ((agora >= interval[0]) && (agora <= interval[1])) {
-        return true
-    } else if (interval[0] > agora)
-        return -1
-    return false
 }
 
 function getUTCAgora() {
@@ -62,8 +145,51 @@ function getFullDateValid(dataI, dataF) { // retorna data inteira de AIRMET e SI
 }
 
 function getUTCDate(date) {
-    let now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
-        date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
-
-    return new Date(now_utc);
+    //let now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
+        // date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
+    let now_utc = new Date(date.getTime() + date.getTimezoneOffset()*60000)
+    return now_utc;
 }
+
+function getIniDataHora(hora) {
+    //201200/201800
+    return hora.substr(2, 2); 
+}
+
+function getFimDataHora(hora) {
+    //201200/201800
+    return hora.substr(9, 2);
+}
+
+function getValidadeMsg(text) {
+    var t = text.replace(/ /g, "")
+    //console.log("t => " + t )
+
+    nuvPatt2 = /\d{6}\/\d{6}/g; //PEGA O intervalo de validade de str sem espaços
+    var t1 = t.match(nuvPatt2)
+    if (t1 && t1.length > 0)
+        return t1[0]
+    else
+        return ""
+}
+
+function msgPertoDoFim(text) {
+    let val = getValidadeMsg(text);
+    let ini = val.split("/")[0]
+    let fim = val.split("/")[1]
+
+    return isCloseToValidOff(ini, fim)
+}
+
+function checaValidadeMsg(texto) {
+
+    let val = getValidadeMsg(texto);
+
+    let ini = val.split("/")[0]
+    let fim = val.split("/")[1]
+    if ((ini !== "") && (fim !== ""))
+        return isValidMsg(ini, fim)
+    else
+        return false
+}
+
