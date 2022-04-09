124e2a6d0a4d8375353abe389cdae89b537bf4a5
brazrei <79328469+brazrei@users.noreply.github.com>
2022-04-09 14:31:28 +0000

    Update airmet.js

--- spago2021/js/airmet.js
+++ spago2021/js/airmet.js
@@ -366,16 +366,13 @@ function getMetar(loc) {
     let met = buscaMetar(arrayMetares, loc) //busca os que tem restrição
     if (met == loc) {//se não, encontrou busca no geral
         try {
-            met = buscaMetar(opener.arrayMetares.slice().reverse(), loc) //pega apenas os metares de uma determinada FIR
-            if (met !== loc) //atualiza a lista geral se achou
-                updateArrayMetaresGeral(loc, met)
-            else
+            met = buscaMetar(opener.arrayMetares.slice().reverse(), loc) // pega apenas os metares de uma determinada FIR
+            if (met == loc) {//se nao achou, atualiza a lista geral 
                 met = buscaMetar(arrayMetaresGeral, loc)
                 if (met !==loc)
                   updateArrayMetaresGeral(loc, met)
             } else
                   updateArrayMetaresGeral(loc, met)
-            
         } catch (e) {
             console.log(e)
         }
