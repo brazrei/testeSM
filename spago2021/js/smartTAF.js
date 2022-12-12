6aea295b374435fff9890636b251ebe70d3c1e1e
brazrei <79328469+brazrei@users.noreply.github.com>
2022-12-12 14:44:03 +0000

    Update smartTAF.js

--- spago2021/js/smartTAF.js
+++ spago2021/js/smartTAF.js
@@ -21,7 +21,6 @@ arrTAFSCimaer.push({ indice: "12 HORAS, (12Z) SAB e DOM", localidades: "SBGP" })
 arrTAFSCimaer.push({ indice: "12 HORAS, (06Z, 12Z) DIARIAMENTE", localidades: "SBPG" });
 arrTAFSCimaer.push({ indice: "12 HORAS, (06Z, 12Z  e 18Z) SEX", localidades: "SBPG" });
 arrTAFSCimaer.push({ indice: "12 HORAS, (06Z e 18Z) DIARIAMENTE", localidades: "SNCP" });
-arrTAFSCimaer.push({ indice: "12 HORAS, (12Z) QUI, SAB E DOM", localidades: "SWKQ" });
 
 function getArrayLength(array) {
     let i = 0
@@ -803,6 +802,7 @@ function GetWebContentTAF(url, primeiraVez) {
 
                 //$("#imgLoad"+idxFIR).attr('src', 'pngs/green-button30.png');
                 let resposta = opener.removeCacheMessage(this.responseText);
+
                 if (isIWXXM(resposta)) {
                     atualizaArrayTAFs(resposta);
                     atualizaStatusConsultaTAF();
@@ -880,6 +880,16 @@ function formataTAFBruto(taf) {
     return tafOut
 
 }
+
+function mostraTAF() {
+    plotaAeroportos()
+}
+
+function getStatusAlertaTAF(){
+    return $('#chkTAF').prop('checked')
+}
+
+
 
 //function getTetoMudanca
 
